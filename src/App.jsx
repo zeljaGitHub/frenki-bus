8f84b7d2b757939a2a79e977f440e64174bdf50c
Zeljko <zel87king2@gmail.com>
2025-04-26 19:44:33 +0200

    ScrollToTopUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Rent from "./pages/Rent";
 import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
-  useEffect(() => {
-    const handleResize = () => {
-      console.log("Širina ekrana:", window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    // Pozovi jednom i odmah
-    handleResize();
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <>
       <Navbar />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Iznajmljivanje-Autobusa" element={<Rent />} />
