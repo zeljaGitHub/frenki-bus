244eace96abd39aa0257a7fb92cdf02541ddbec5
Zeljko <zel87king2@gmail.com>
2025-04-26 21:54:04 +0200

    scroll to top update 3

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -5,11 +5,11 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    const handle = setTimeout(() => {
-      window.scrollTo(0, 0);
-    }, 100); // Wait 100ms for new page to render
-
-    return () => clearTimeout(handle); // Cleanup timeout if component unmounts quickly
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant", // instead of 'smooth'
+    });
   }, [pathname]);
 
   return null;
